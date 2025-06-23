986af4f968d78c09b63337a6461478f04ccc9d31
Дмитрий <dmitrij@192.168.100.3>
2025-06-23 13:34:30 +0300

    Adding iShop project

---
+++ ishop0/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(
+  <Shop name="iMy" address="1234 Sunset Blvd Los Angeles, CA 90026 USA"/>, 
+  document.getElementById('container') 
+);
---
+++ ishop0/components/Shop.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className='Shop'>
+          <h1>Магазин: {this.props.name}</h1>
+          <p>Адрес: {this.props.address}</p>
+      </div>
+    );
+  }
+
+}
+
+export default Shop;
