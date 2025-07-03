e1a8f6778eff0f687dd0ca073f3df6e1789e0825
Екатерина <dmitrij@MacBook-Air-Dmitrij.local>
2025-07-03 12:10:24 +0200

    Adding border and background for selected product

--- ishop0/components/Product.js
+++ ishop0/components/Product.js
@@ -3,15 +3,19 @@ import './Product.css';
 
 class Product extends React.Component {
   render() {
-    const { name, price, imageUrl, stock } = this.props.product;
+    const { product, isSelected, onClick } = this.props;
+    const { name, price, imageUrl, stock } = product;
 
     return (
-      <div className="Product">
+      <div
+        className={`Product ${isSelected ? 'Selected' : ''}`}
+        onClick={onClick}
+      >
         <img src={imageUrl} alt={name} className="ProductImage" />
         <div className="ProductInfo">
           <h3>{name}</h3>
-          <p className='Price'>{price} $</p>
-          <p className='Stock'>В наличии: <span>{stock}</span></p>
+          <p className="Price">{price} $</p>
+          <p className="Stock">В наличии: <span>{stock}</span></p>
         </div>
       </div>
     );
--- ishop0/components/Shop.js
+++ ishop0/components/Shop.js
@@ -3,25 +3,43 @@ import Product from './Product';
 import './Shop.css';
 
 class Shop extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedProductId: null,
+    };
+  }
+
+  handleProductClick = (productId) => {
+    this.setState((prevState) => ({
+      selectedProductId:
+        prevState.selectedProductId === productId ? null : productId,
+    }));
+  };
 
   render() {
     const { name, address, products } = this.props;
+    const { selectedProductId } = this.state;
 
     return (
-      <div className='Shop'>
-         <div className='ShopInformation'>
-            <h1>Магазин: {name}</h1>
-            <p>Адрес: {address}</p>
+      <div className="Shop">
+        <div className="ShopInformation">
+          <h1>Магазин: {name}</h1>
+          <p>Адрес: {address}</p>
         </div>
-          <div className="ProductList">
-            {products.map((product) => (
-            <Product key={product.id} product={product} />
+        <div className="ProductList">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+              isSelected={selectedProductId === product.id}
+              onClick={() => this.handleProductClick(product.id)}
+            />
           ))}
         </div>
       </div>
     );
   }
-
 }
 
 export default Shop;
